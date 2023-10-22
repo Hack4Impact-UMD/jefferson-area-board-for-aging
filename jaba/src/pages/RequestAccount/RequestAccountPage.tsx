6301221cf095d1453318df311760d5585e7f11f0
Krushi Shah <krushishah@wireless-10-105-151-212.umd.edu>
2023-10-22 13:55:41 -0400

    editing frontend, connecting popup, and form validation

--- jaba/src/pages/RequestAccount/RequestAccountPage.tsx
+++ jaba/src/pages/RequestAccount/RequestAccountPage.tsx
@@ -1,83 +1,215 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import './RequestAccountPage.css';
-import ConfirmationPage from './RequestAccountConfirmation';
+import './popup.css';
+import requestIconSuccess from '../../assets/requestIconSuccess.svg';
+
 const RequestAccountPage = () => {
-  
+  const [modal, setModal] = useState(false);
+
+  const [formData, setFormData] = useState({
+    fullname: '',
+    email: '',
+    phone: '',
+    organization: '',
+    position: '',
+    address: '',
+    accountType: 'Admin',
+    urgency: 'Regular',
+    additionalInfo: '',
+  });
+
+  const toggleModal = () => {
+    setModal(!modal);
+  };
+
+  const handleFormSubmit = (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+    if (formData.fullname.trim() !== '' 
+        && formData.email.trim() !== ''  
+        && formData.phone.trim() !== ''
+        && formData.organization.trim() !== '' 
+        && formData.position.trim() !== ''
+        && formData.address.trim() !== ''
+        && formData.accountType.trim() !== ''
+        && formData.urgency.trim() !== '') {
+      console.log('Form Data:', formData);
+    toggleModal();
+    } else {
+      alert('Please fill out all required fields.');
+    }
+  };
+
+
+  const handleInputChange = (field: string, value: string) => {
+    setFormData({
+      ...formData,
+      [field]: value,
+    });
+  };
+
+  const resetPage = () => {
+    window.location.reload();
+  };
+
+  if (modal) {
+    document.body.classList.add('active-modal');
+  } else {
+    document.body.classList.remove('active-modal');
+  }
+
   return (
     <div className="request-page">
       <div className="request-header">
-
         <h1 className="req-title">Request for Account</h1>
 
-        <div id="format-header">
-          <p className = "req-text">
-            To request an account, please fill 
-            out the form below with your details and requirements. 
-            An administrator will review your request and create 
-            your account accordingly.
+        <div id="format-header" className="heading-container">
+          <p className="req-text">
+            To request an account, please fill out the form below with your details and requirements. An administrator will review your request and create your account accordingly.
           </p>
         </div>
 
-        <form>
+        <form onSubmit={handleFormSubmit}>
           <div className="fields">
-            <div className="req-entry" style={{gridArea:'fullname'}}>
+            <div className="req-entry" style={{ gridArea: 'fullname' }}>
               <p>Full name</p>
-              <textarea className = "input-text" placeholder='Enter your full name'></textarea>
+              <textarea
+                className="input-text"
+                placeholder="Enter your full name"
+                value={formData.fullname}
+                onChange={(e) => handleInputChange('fullname', e.target.value)}
+              ></textarea>
             </div>
 
-            <div className="req-entry" style={{gridArea: 'email'}}>
+            <div className="req-entry" style={{ gridArea: 'email' }}>
               <p>Email</p>
-              <textarea className = "input-text" placeholder='Enter your email linked to your account'></textarea>
+              <textarea
+                className="input-text"
+                placeholder="Enter your email linked to your account"
+                value={formData.email}
+                onChange={(e) => handleInputChange('email', e.target.value)}
+              ></textarea>
             </div>
 
-            <div className="req-entry" style={{gridArea: "phone"}}>
+            <div className="req-entry" style={{ gridArea: 'phone' }}>
               <p>Phone</p>
-              <textarea className = "input-text" placeholder='Enter your phone number'></textarea>
+              <textarea
+                className="input-text"
+                placeholder="Enter your phone number"
+                value={formData.phone}
+                onChange={(e) => handleInputChange('phone', e.target.value)}
+              ></textarea>
             </div>
 
-            <div className="req-entry" style={{gridArea: "organization"}}>
+            <div className="req-entry" style={{ gridArea: 'organization' }}>
               <p>Organization</p>
-              <textarea className = "input-text" placeholder="Enter your organization's name"></textarea>
+              <textarea
+                className="input-text"
+                placeholder="Enter your organization's name"
+                value={formData.organization}
+                onChange={(e) => handleInputChange('organization', e.target.value)}
+              ></textarea>
             </div>
 
-            <div className="req-entry" style={{gridArea: "position"}}>
+            <div className="req-entry" style={{ gridArea: 'position' }}>
               <p>Position at Organization</p>
-              <textarea className = "input-text" placeholder='Enter your role'></textarea>
+              <textarea
+                className="input-text"
+                placeholder="Enter your role"
+                value={formData.position}
+                onChange={(e) => handleInputChange('position', e.target.value)}
+              ></textarea>
             </div>
 
-            <div className="req-entry" style={{gridArea:"address"}}>
+            <div className="req-entry" style={{ gridArea: 'address' }}>
               <p>Address</p>
-              <textarea className = "input-text" placeholder='Enter your residential address'></textarea>
+              <textarea
+                className="input-text"
+                placeholder="Enter your residential address"
+                value={formData.address}
+                onChange={(e) => handleInputChange('address', e.target.value)}
+              ></textarea>
             </div>
 
-            <div className="req-options" style={{gridArea:"create"}}>
-              <div style={{display: "flex", alignItems: "center"}}>
+            <div className="req-options" style={{ gridArea: 'create' }}>
+              <div>
                 <p>Create Account as:</p>
-                <button className ="reg-btn">Admin</button>
-                <button className ="reg-btn">User</button>
+                <div
+                  className={`rounded-button ${formData.accountType === 'Admin' ? 'selected' : ''}`}
+                  onClick={() => handleInputChange('accountType', 'Admin')}
+                >
+                  Admin
+                </div>
+
+                <div
+                  className={`rounded-button ${formData.accountType === 'User' ? 'selected' : ''}`}
+                  onClick={() => handleInputChange('accountType', 'User')}
+                >
+                  User
+                </div>
               </div>
             </div>
 
-            <div className="req-options" style={{gridArea:"priority"}}>
-              <div style={{display: "flex", alignItems: "center"}}>
+            <div className="req-options" style={{ gridArea: 'priority' }}>
+              <div className="priority-options">
                 <p>Priority:</p>
-                <button className="reg-btn">Regular</button>
-                <button className="reg-btn">Urgent</button>
+                <div
+                  className={`rounded-button ${formData.urgency === 'Regular' ? 'selected' : ''}`}
+                  onClick={() => handleInputChange('urgency', 'Regular')}
+                >
+                  Regular
+                </div>
+
+                <div
+                  className={`rounded-button ${formData.urgency === 'Urgent' ? 'selected' : ''}`}
+                  onClick={() => handleInputChange('urgency', 'Urgent')}
+                >
+                  Urgent
+                </div>
               </div>
             </div>
 
-            <div className="req-entry" style={{gridArea:"additional"}}>
+            <div className="req-entry" style={{ gridArea: 'additional' }}>
               <p>Additional Information</p>
-              <textarea className = "input-text" placeholder='Use this space to enter any extra information or specific requirements they have for the account.'></textarea>
+              <textarea
+                className="input-text"
+                placeholder="Use this space to enter any extra information or specific requirements they have for the account."
+                value={formData.additionalInfo}
+                onChange={(e) => handleInputChange('additionalInfo', e.target.value)}
+              ></textarea>
             </div>
 
-            <div className="submit" style={{gridArea:"submit_btn"}}>
-              <button type = "submit" className="submit-response">Submit Request</button>
+            <div className="submit" style={{ gridArea: 'submit_btn' }}>
+              <button type="submit" className="submit-response">
+                Submit Request
+              </button>
             </div>
           </div>
         </form>
+
+        {modal && (
+          <div className="modal">
+            <div onClick={toggleModal} className="overlay"></div>
+            <div className="modal-content">
+              <h2>Request Submitted Successfully!</h2>
+
+              <p className="description">
+                Please allow a few business days for our admin to process your application. Keep an eye on your email for any additional information or questions from our admins.
+              </p>
+
+              <img src={requestIconSuccess} alt="checkmark" />
+
+              <p>
+                <span className="submitted">
+                  Submit another request?{' '}
+                  <a className="click-here" onClick={resetPage}>
+                    <strong>Click here</strong>
+                  </a>
+                </span>
+              </p>
+            </div>
+          </div>
+        )}
       </div>
-      <ConfirmationPage/>
     </div>
   );
 };
