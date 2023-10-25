7e0ea8829a6119aa6a3d44de95e6b9ed4c6b7f7a
Sai Pranav Theerthala <sptheerthala@gmail.com>
2023-10-25 16:22:04 -0400

    completed front end to both pages

--- jaba/src/App.tsx
+++ jaba/src/App.tsx
@@ -1,9 +1,11 @@
 import ExamplePage from "./pages/ExamplePage/ExamplePage";
+import ForgotPassword from "./pages/ForgotPasswordFrontEnd";
+import ResetPassword from "./pages/ResetPasswordFrontEnd";
 
 function App(): JSX.Element {
 
   return (
-    <ExamplePage/>
+    <><ForgotPassword /><ResetPassword /></>
   );
 }
 
