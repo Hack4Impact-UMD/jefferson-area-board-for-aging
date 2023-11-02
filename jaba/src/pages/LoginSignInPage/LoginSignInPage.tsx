c7e7753e15d672261f98f265d72eb5f784837dfc
tkomm4 <kommuri.tarun@gmail.com>
2023-11-02 18:44:26 -0400

    finished welcome page. missing the show-password component & switching between user and admin log in using props component

--- jaba/src/pages/LoginSignInPage/LoginSignInPage.tsx
+++ jaba/src/pages/LoginSignInPage/LoginSignInPage.tsx
@@ -1,6 +1,9 @@
 import { useState, useEffect } from 'react';
 import styles from './LoginSignInPage.module.css';
 import SignIn from '../../assets/signin.png';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 <head>
 <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Text&family=Inter:wght@600&family=Poppins:wght@500&display=swap" rel="stylesheet"></link>
@@ -25,12 +28,27 @@ const LoginSignInPage = () => {
               <p className={styles.labelProperties}>Email</p>
                 <input className={styles.textBox} placeholder="Enter your email linked to your account" />
               <p className={styles.labelProperties}>Password</p>
-                <input className={styles.textBox} placeholder="Enter your email linked to your account" />
-              
+                <input className={styles.textBox} placeholder="Enter your Password" />
             </div> 
-              <div className={styles.loginBox}>
-                <button className={styles.loginButton}>Login</button>
+            <div className={styles.flexContainer}>
+              <div className={styles.checkboxProperty}>
+              <FormControlLabel
+                control={<Checkbox value="remember"  sx={{ '& .MuiSvgIcon-root': { fontSize: 22 } }} />}
+                label={
+                  <span style={{ color: '#4D4D4D', fontFamily: 'Inter', fontSize: '14px'}}>
+                    Remember me
+                  </span>
+                }
+              />
               </div>
+              <div className={styles.forgotPassword}> 
+                <p><a href="gotoresgisterpagefromhere" className={styles.forgotPassword}>Forgot Password?</a></p>
+              </div>
+    
+            </div>
+            <div className={styles.loginBox}>
+              <button className={styles.loginButton}>Login</button>
+            </div>
 
               <div className={styles.registerBlurb}>
                 <p>Not a User? <a href="gotoresgisterpagefromhere" className={styles.boldText}><b>Switch to Admin Login</b></a></p>
