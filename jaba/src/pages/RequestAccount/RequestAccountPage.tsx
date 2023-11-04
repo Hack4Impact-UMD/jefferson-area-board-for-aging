512b674ddf2b45bbd9711ea958093a28d3128218
Andrew <andrewyang1015@gmail.com>
2023-11-04 03:27:32 -0400

    6-request-account-page Adjusted to style conventions

--- jaba/src/pages/RequestAccount/RequestAccountPage.tsx
+++ jaba/src/pages/RequestAccount/RequestAccountPage.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import './RequestAccountPage.css';
+import styles from './RequestAccountPage.module.css';
 import './popup.css';
 import requestIconSuccess from '../../assets/requestIconSuccess.svg';
 
@@ -52,129 +52,137 @@ const RequestAccountPage = () => {
   };
 
   if (modal) {
-    document.body.classList.add('active-modal');
+    document.body.classList.add('activeModal');
   } else {
-    document.body.classList.remove('active-modal');
+    document.body.classList.remove('activeModal');
   }
 
   return (
-    <div className="request-page">
-      <div className="request-header">
-        <h1 className="req-title">Request for Account</h1>
+    <div className={styles.requestPage}>
+      <div className={styles.requestHeader}>
+        <h1 className={styles.reqTitle}>Request for Account</h1>
 
-        <div id="format-header" className="heading-container">
-          <p className="req-text">
+        <div id={styles.formatHeader} className={styles.headingContainer}>
+          <p className={styles.reqText}>
             To request an account, please fill out the form below with your details and requirements. An administrator will review your request and create your account accordingly.
           </p>
         </div>
       </div>
-      <div className="request-body">
+      <div className={styles.requestBody}>
         <form onSubmit={handleFormSubmit}>
-          <div className="fields">
-            <div className="req-entry" style={{ gridArea: 'fullname' }}>
-              <p className="req-descrip">Full name</p>
+          <div className={styles.fields}>
+            <div className={styles.reqEntry} style={{ gridArea: 'fullname' }}>
+              <p className={styles.reqDescrip}>Full name</p>
               <textarea
-                className="input-text"
+                className={styles.inputText}
                 placeholder="Enter your full name"
                 value={formData.fullname}
                 onChange={(e) => handleInputChange('fullname', e.target.value)}
               ></textarea>
             </div>
 
-            <div className="req-entry" style={{ gridArea: 'email' }}>
-              <p className="req-descrip">Email</p>
+            <div className={styles.reqEntry} style={{ gridArea: 'email' }}>
+              <p className={styles.reqDescrip}>Email</p>
               <textarea
-                className="input-text"
+                className={styles.inputText}
                 placeholder="Enter your email linked to your account"
                 value={formData.email}
                 onChange={(e) => handleInputChange('email', e.target.value)}
               ></textarea>
             </div>
 
-            <div className="req-entry" style={{ gridArea: 'phone' }}>
-              <p className="req-descrip">Phone</p>
+            <div className={styles.reqEntry} style={{ gridArea: 'phone' }}>
+              <p className={styles.reqDescrip}>Phone</p>
               <textarea
-                className="input-text"
+                className={styles.inputText}
                 placeholder="Enter your phone number"
                 value={formData.phone}
                 onChange={(e) => handleInputChange('phone', e.target.value)}
               ></textarea>
             </div>
 
-            <div className="req-entry" style={{ gridArea: 'organization' }}>
-              <p className="req-descrip">Organization</p>
+            <div className={styles.reqEntry} style={{ gridArea: 'organization' }}>
+              <p className={styles.reqDescrip}>Organization</p>
               <textarea
-                className="input-text"
+                className={styles.inputText}
                 placeholder="Enter your organization's name"
                 value={formData.organization}
                 onChange={(e) => handleInputChange('organization', e.target.value)}
               ></textarea>
             </div>
 
-            <div className="req-entry" style={{ gridArea: 'position' }}>
-              <p className="req-descrip">Position at Organization</p>
+            <div className={styles.reqEntry} style={{ gridArea: 'position' }}>
+              <p className={styles.reqDescrip}>Position at Organization</p>
               <textarea
-                className="input-text"
+                className={styles.inputText}
                 placeholder="Enter your role"
                 value={formData.position}
                 onChange={(e) => handleInputChange('position', e.target.value)}
               ></textarea>
             </div>
 
-            <div className="req-entry" style={{ gridArea: 'address' }}>
-              <p className="req-descrip">Address</p>
+            <div className={styles.reqEntry} style={{ gridArea: 'address' }}>
+              <p className={styles.reqDescrip}>Address</p>
               <textarea
-                className="input-text"
+                className={styles.inputText}
                 placeholder="Enter your residential address"
                 value={formData.address}
                 onChange={(e) => handleInputChange('address', e.target.value)}
               ></textarea>
             </div>
 
-            <div className="req-options" style={{ gridArea: 'options' }}>
-              <div className="account-btn">
-                <p className="req-descrip">Create Account as:</p>
+            <div className={styles.reqOptions} style={{ gridArea: 'options' }}>
+              <div className={styles.accountBtn}>
+                <p className={styles.reqDescrip}>Create Account as:</p>
                 <div
-                  className={`rounded-button ${formData.accountType === 'Admin' ? 'selected' : ''}`}
+                  className={
+                    `${styles.roundedButton} ${formData.accountType === 'Admin' ? styles.selected : ''}`
+                  }
                   onClick={() => handleInputChange('accountType', 'Admin')}
                 >
                   Admin
                 </div>
 
                 <div
-                  className={`rounded-button ${formData.accountType === 'User' ? 'selected' : ''}`}
+                  className={
+                    `${styles.roundedButton} ${formData.accountType === 'User' ? styles.selected : ''}`
+                  }
                   onClick={() => handleInputChange('accountType', 'User')}
                 >
                   User
                 </div>
               </div>
-              <div className="priority-options">
-                <p className='req-descrip'>Priority:</p>
+              <div className={styles.priorityOptions}>
+                <p className={styles.reqDescrip}>Priority:</p>
                 <div
-                  className={`rounded-button ${formData.urgency === 'Regular' ? 'selected' : ''}`}
+                  className={
+                    `${styles.roundedButton} ${formData.urgency === 'Regular' ? styles.selected : ''}`
+                  }
                   onClick={() => handleInputChange('urgency', 'Regular')}>
                   Regular
                 </div>
                 <div
-                  className={`rounded-button ${formData.urgency === 'Urgent' ? 'selected' : ''}`}
+                  className={
+                    `${styles.roundedButton} ${formData.urgency === 'Urgent' ? styles.selected : ''}`
+                  }
                   onClick={() => handleInputChange('urgency', 'Urgent')}>
                   Urgent
                 </div>
               </div>
             </div>
 
-            <div className="req-entry" style={{ gridArea: 'additional' }}>
-              <p className="priority-options">Additional Information</p>
+            <div className={styles.reqEntry} style={{ gridArea: 'additional' }}>
+              <p className={styles.priorityOptions}>Additional Information</p>
               <textarea
-                className="input-text-additional"
+                className={styles.inputTextAdditional}
                 placeholder="Use this space to enter any extra information or specific requirements they have for the account."
                 value={formData.additionalInfo}
                 onChange={(e) => handleInputChange('additionalInfo', e.target.value)}
               ></textarea>
             </div>
 
-            <div className="submit" style={{ gridArea: 'submit_btn' }}>
-              <button type="submit" className="submit-response">
+            <div className={styles.submit} style={{ gridArea: 'submit_btn' }}>
+              <button type="submit" className={styles.submitResponse}>
                 Submit Request
               </button>
             </div>
@@ -184,7 +192,7 @@ const RequestAccountPage = () => {
         {modal && (
           <div className="modal">
             <div onClick={toggleModal} className="overlay"></div>
-            <div className="modal-content">
+            <div className="modalContent">
               <h2>Request Submitted Successfully!</h2>
 
               <p className="description">
@@ -196,7 +204,7 @@ const RequestAccountPage = () => {
               <p>
                 <span className="submitted">
                   Submit another request?{' '}
-                  <a className="click-here" onClick={resetPage}>
+                  <a className="clickHere" onClick={resetPage}>
                     <strong>Click here</strong>
                   </a>
                 </span>
