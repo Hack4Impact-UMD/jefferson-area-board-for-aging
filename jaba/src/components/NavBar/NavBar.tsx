c24e1f4335c76f9ab826227b0697699d8e635fbb
Meher Bhamra <mbhamra@terpmail.umd.edu>
2023-11-05 10:06:42 -0500

    added code to highlight (and change font color/icon color of) a section of the navbar when clicked. also made adjustments to css for this.

--- jaba/src/components/NavBar/NavBar.tsx
+++ jaba/src/components/NavBar/NavBar.tsx
@@ -1,4 +1,4 @@
-import navStyles from './NavBar.module.css';
+import styles from './NavBar.module.css';
 import { Component } from 'react';
 import HomeBlack from '../../assets/icons/homeBlack.png';
 import SettingsBlack from '../../assets/icons/settingsBlack.png';
@@ -12,35 +12,35 @@ import LogoutWhite from '../../assets/icons/logoutWhite.png';
 class NavBar extends Component {
     render() {
         return (
-            <div className={navStyles.sidebar}>
-                <h2 className={navStyles.headerStyle}>[name]</h2>
+            <div className={styles.sidebar}>
+                <h2 className={styles.headerStyle}>[ n a m e ]</h2>
                 <div>
                     <ul>
-                        <li>
-                            <a className={navStyles.textStyle} href="/">
-                                <img src={HomeBlack}/>
+                        <li id="homeBlock">
+                            <a className={window.location.href == 'http://localhost:3000/' ? styles.blockStyle : styles.textStyle} href="/">
+                                <img id="homeImg" src ={window.location.href == 'http://localhost:3000/' ? HomeWhite : HomeBlack}/>
                                 <span>Home</span>
                             </a>
                         </li>
-                        <li>
-                            <a className={navStyles.textStyle} href="/settings">
-                                <img src={SettingsBlack}/>
+                        <li id="settingsBlock">
+                            <a className={window.location.href == 'http://localhost:3000/settings' ? styles.blockStyle : styles.textStyle} href="/settings">
+                                <img id="settingsImg" src ={window.location.href == 'http://localhost:3000/settings' ? SettingsWhite : SettingsBlack}/>
                                 <span>Settings</span>
                             </a>
                         </li>
-                        <li>
-                            <a className={navStyles.textStyle} href="/users">
-                                <img src={UsersBlack}/>
+                        <li id="usersBlock">
+                            <a className={window.location.href == 'http://localhost:3000/users' ? styles.blockStyle : styles.textStyle} href="/users">
+                                <img id="usersImg" src ={window.location.href == 'http://localhost:3000/users' ? UsersWhite : UsersBlack}/>
                                 <span>Users</span>
                             </a>
                         </li>
                     </ul>
                 </div>
-                <div className={navStyles.logout}>
+                <div className={styles.logout}>
                     <ul> 
-                        <li>
-                            <a className={navStyles.textStyle} href="/logout?">
-                                <img src={LogoutBlack}/>
+                        <li id="logoutBlock">
+                            <a id="logoutLink" className={styles.textStyle} href="/logout?">
+                                <img id="logoutImg" src={LogoutBlack}/>
                                 <span>Logout</span>
                             </a>
                         </li>
