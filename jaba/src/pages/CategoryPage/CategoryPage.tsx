1a3a90ec01e00f8fa1f2fc9dbc554bc16af40271
Meher Bhamra <mbhamra@terpmail.umd.edu>
2023-11-11 22:31:28 -0500

    added preliminary files/pages

---
+++ jaba/src/pages/CategoryPage/CategoryPage.tsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react';
+import styles from './CategoryPage.module.css';
+
+const CategoryPage = () => {
+    const handleClick = () => {
+    };
+
+    return (
+      <>
+        
+      </>
+    );
+  }
+  
+  export default CategoryPage;
