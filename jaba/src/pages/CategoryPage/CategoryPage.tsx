411858aa03b1f032ded1cec745a4ecc8ab64e325
Meher Bhamra <mbhamra@terpmail.umd.edu>
2023-11-15 17:49:12 -0500

    created basic backgrounds for page

--- jaba/src/pages/CategoryPage/CategoryPage.tsx
+++ jaba/src/pages/CategoryPage/CategoryPage.tsx
@@ -1,13 +1,47 @@
 import { useState, useEffect } from 'react';
 import styles from './CategoryPage.module.css';
+import NavBar from '../../components/NavBar/NavBar';
 
 const CategoryPage = () => {
     const handleClick = () => {
     };
 
+    const isAdmin = true;
+    const props = {isAdmin};
+
     return (
       <>
-        
+        <NavBar {...props}/>
+        <div className={styles.background}>
+          <div className={styles.innerBackground}>
+            <div className={styles.backButton}>
+
+            </div>
+            <div className={styles.innerBox}>
+              <div className={styles.topPart}>
+                <div className={styles.screens}>
+                </div>
+                <div className={styles.screens}>
+                </div>
+                <div className={styles.screens}>
+                </div>
+              </div>
+              <div className={styles.bottomPart}>
+                <div className={styles.list}>
+                  <div className={styles.screens}>
+                  </div>
+                  <div className={styles.screens}>
+                  </div>
+                  <div className={styles.screens}>
+                  </div>
+                </div>
+                <div className={styles.bottomNav}>
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </>
     );
   }
