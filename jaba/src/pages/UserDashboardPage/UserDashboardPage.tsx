b25b33cdc70e10b87b3c119c2b21934e3cbd47d3
Megan Yu <yu.megan@gmail.com>
2023-11-16 16:56:38 -0500

    mui multiselect names as filler data

--- jaba/src/pages/UserDashboardPage/UserDashboardPage.tsx
+++ jaba/src/pages/UserDashboardPage/UserDashboardPage.tsx
@@ -3,6 +3,23 @@ import styles from './UserDashboardPage.module.css';
 import NavBar from '../../components/NavBar/NavBar';
 import AdminHomeDashboard from '../../assets/adminhomedashboard.png';
 import FilterIcon from '../../assets/filtericon.svg';
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import React from 'react';
+
+const names = [
+    'Oliver Hansen',
+    'Van Henry',
+    'April Tucker',
+    'Ralph Hubbard',
+    'Omar Alexander',
+    'Carlos Abbott',
+    'Miriam Wagner',
+    'Bradley Wilkerson',
+    'Virginia Andrews',
+    'Kelly Snyder',
+  ];
 
 const UserDashboardPage = () => {
     const handleClick = () => {
@@ -10,6 +27,20 @@ const UserDashboardPage = () => {
 
     const isAdmin = true;
     const props = {isAdmin};
+
+    // task 11 start
+    const [personName, setPersonName] = React.useState<string[]>([]);
+    const handleChangeMultiple = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const { options } = event.target;
+    const value: string[] = [];
+    for (let i = 0, l = options.length; i < l; i += 1) {
+      if (options[i].selected) {
+        value.push(options[i].value);
+      }
+    }
+    setPersonName(value);
+    };
+    // task 11 end
 
     return (
       <>
@@ -26,6 +57,32 @@ const UserDashboardPage = () => {
                 <div className={styles.bottomPart}>
                     <div className={styles.screens}>
                         <p className={styles.header}>Category</p>
+                        {/* task 11 start*/}
+                        <div>
+                            <FormControl sx={{ m: 1, minWidth: 120, maxWidth: 300 }}>
+                                <InputLabel shrink htmlFor="select-multiple-native">
+                                Native
+                                </InputLabel>
+                                <Select
+                                multiple
+                                native
+                                value={personName}
+                                // @ts-ignore Typings are not considering `native`
+                                onChange={handleChangeMultiple}
+                                label="Native"
+                                inputProps={{
+                                    id: 'select-multiple-native',
+                                }}
+                                >
+                                {names.map((name) => (
+                                    <option key={name} value={name}>
+                                    {name}
+                                    </option>
+                                ))}
+                                </Select>
+                            </FormControl>
+                        </div>
+                        {/* task 11 end */}
                     </div>
                     <div className={styles.middleImageDiv}>
                         <img className={styles.middleImage} src={AdminHomeDashboard}/>
