1682ec76a8b285b405b7ad0ad5319fa7bb3c7986
Meher Bhamra <mbhamra@terpmail.umd.edu>
2023-11-17 00:27:54 -0500

    added area to go back

--- jaba/src/pages/CategoryPage/CategoryPage.tsx
+++ jaba/src/pages/CategoryPage/CategoryPage.tsx
@@ -1,14 +1,19 @@
 import { useState, useEffect } from 'react';
 import styles from './CategoryPage.module.css';
 import NavBar from '../../components/NavBar/NavBar';
-import Edit from '../../assets/icons/edit.png'
-import Trash from '../../assets/icons/trash.png'
+import Edit from '../../assets/icons/edit.png';
+import Trash from '../../assets/icons/trash.png';
+import Back from '../../assets/icons/backIcon.png';
 
 const CategoryPage = () => {
-    const handleClick = () => {
-    };
-
-    // const [categories, setCategories] = useState<Category[]>([]);
+    const categories = [
+      {name: 'Primary Category Name', classification: 'None'}, 
+      {name: 'Mortgage', classification: 'Finance'}, 
+      {name: 'Social Services', classification: 'Child Care'},
+      {name: 'Primary Category Name2', classification: 'None'}, 
+      {name: 'Mortgage2', classification: 'Finance'}, 
+      {name: 'Social Services2', classification: 'Child Care'}
+    ]
 
     // useEffect(() => {
     //   getCategories()
@@ -31,8 +36,11 @@ const CategoryPage = () => {
         <NavBar {...props}/>
         <div className={styles.background}>
           <div className={styles.innerBackground}>
-            <div className={styles.backButton}>
-
+            <div className={styles.backLink}>
+              <a href=''>
+                <img src={Back}/>
+                <span>Back</span>
+              </a>
             </div>
             <div className={styles.innerBox}>
               <div className={styles.topPart}>
@@ -44,18 +52,24 @@ const CategoryPage = () => {
               </div>
               <div className={styles.bottomPart}>
                 <tbody className={styles.bottomPart2}>
-                  <tr className={styles.row}>
-                    <td className={styles.screens}>Primary Category Name</td>
-                    <td className={styles.screens}>None</td>
-                    <td className={styles.screens}>
-                      <a className={styles.icons} href="/edit">
-                        <img src ={Edit}/>
-                      </a>
-                      <a className={styles.icons} href="/delete?">
-                        <img src ={Trash}/>
-                      </a>
-                    </td>
-                  </tr>
+                  {
+                  categories.map(category => {
+                    return (
+                      <tr className={styles.row}>
+                        <td className={styles.screens}>{category.name}</td>
+                        <td className={styles.screens}>{category.classification}</td>
+                        <td className={styles.screens}>
+                          <a className={styles.icons} href="/edit">
+                            <img src={Edit}/>
+                          </a>
+                          <a className={styles.icons} href="/delete?">
+                            <img src={Trash}/>
+                          </a>
+                        </td>
+                      </tr>
+                      )
+                    })
+                  }
                 </tbody>
                 <div className={styles.bottomNav}>
                 </div>
