7de7178e8a112b80a754459971c784d0fcf32dda
Megan Yu <yu.megan@gmail.com>
2023-11-28 16:23:16 -0500

    Include National Services toggle frontend need to implement backend and style

--- jaba/src/pages/UserDashboardPage/RegionFilter/RegionFilter.tsx
+++ jaba/src/pages/UserDashboardPage/RegionFilter/RegionFilter.tsx
@@ -1,5 +1,10 @@
 import * as React from 'react';
 import styles from "./RegionFilter.module.css"
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
 
 const RegionFilter = () => {
 
@@ -27,7 +32,22 @@ const RegionFilter = () => {
     ]
     
     return (
-        <p>hi</p>
+        <div>
+            <FormControl>
+                <FormLabel id="demo-row-radio-buttons-group-label">Include National Services</FormLabel>
+                <RadioGroup
+                    row
+                    aria-labelledby="demo-row-radio-buttons-group-label"
+                    name="row-radio-buttons-group"
+                >
+                    <FormControlLabel value="yes" control={<Radio />} label="Yes" />
+                    <FormControlLabel value="no" control={<Radio />} label="No" />
+
+                </RadioGroup>
+            </FormControl>
+
+            
+        </div>
     )
 
 }
